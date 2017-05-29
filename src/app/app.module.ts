de0e6afd42ec7471fdcd317b32e0abf41d347a35
Anand <anand.cse.imps@gmail.com>
2017-05-30 00:25:18 +0530

    adding checkboxes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import {ApiService} from './services/api.service';
@@ -10,7 +10,6 @@ import {ApiService} from './services/api.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule
   ],
   providers: [ApiService],

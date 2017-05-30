63bd4dd0a808f2a157b194766dd0b8878c168f08
Anand <anand.cse.imps@gmail.com>
2017-05-30 12:37:33 +0530

    adding dropDown in HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import {ApiService} from './services/api.service';
+import { FormsModule }   from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    FormsModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
